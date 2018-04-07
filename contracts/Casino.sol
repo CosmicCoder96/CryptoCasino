7cc4d599b3d0e74abc0a85a703d6a343094836fe
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-04-07 10:44:52 +0530

    Complete the contact

--- contracts/Casino.sol
+++ contracts/Casino.sol
@@ -2,11 +2,73 @@ pragma solidity 0.4.21;
 
 contract Casino {
     address public owner;
-    function Casino() public {
+    uint256 public minimumBet;
+    uint256 public totalBet;
+    uint256 public numberOfBets;
+    uint256 public maxAmountOfBets = 100;
+    address[] public players;
+
+    struct Player {
+        uint256 amountBet;
+        uint256 numberSelected;
+    }
+
+    mapping(address => Player) public playerInfo;
+
+    function() public payable {}
+
+    function Casino(uint256 _minimumBet) public {
         owner = msg.sender;
+        if(_minimumBet != 0) {
+            minimumBet = _minimumBet;
+        }
     }
 
     function kill() public {
         if (msg.sender == owner) selfdestruct(owner);
+    }
+
+    function  bet(uint256 _numberSelected) public payable {
+        require(!checkPlayerExists(msg.sender));
+        require(_numberSelected >= 1 && _numberSelected <= 10);
+        require(msg.value >= minimumBet);
+        playerInfo[msg.sender].amountBet = msg.value;
+        playerInfo[msg.sender].numberSelected = _numberSelected;
+        numberOfBets++;
+        players.push(msg.sender);
+        totalBet += msg.value;
+        if(numberOfBets >= maxAmountOfBets) generateNumberWinner();
+
+    }
+
+    function checkPlayerExists(address _player) public constant returns (bool) {
+        for (uint256 i = 0; i < players.length; i++){
+            if(players[i] == _player) return true;
+        }
+        return false;
+    }
+
+    function generateNumberWinner() public {
+        uint256 numberGenerated = block.number%10 + 1;
+        distributePrizes(numberGenerated);
+    }
+
+    function distributePrizes(uint256 _numberWinner) public{
+        address[100] memory winners;
+        uint256 count = 0;
+        for(uint256 i = 0; i < players.length; i++) {
+            address playerAddress = players[i];
+            if(playerInfo[playerAddress].numberSelected == _numberWinner) {
+                winners[count] = playerAddress;
+                count ++;
+            }
+            delete playerInfo[playerAddress];
+        }
+        players.length = 0;
+        uint256 winnerEtherAmount = totalBet/winners.length;
+        for(uint256 j = 0; j < count; j++) {
+            if(winners[j] != address(0))
+                winners[j].transfer(winnerEtherAmount);
+        }
     }
 }
